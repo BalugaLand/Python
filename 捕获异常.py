873ef37d2ba7d637e7556f37fa08c519a532b6d9
BalugaLand <114586562+BalugaLand@users.noreply.github.com>
2022-11-15 22:05:14 +0800

    Update 捕获异常.py

--- 捕获异常.py
+++ 捕获异常.py
@@ -19,3 +19,18 @@ try:                                        # 捕获多个异常
     b = 1/0
 except(NameError,ZeroDivisionError) as e:   # 没有变量a 或者 除以0无意义 的异常
     print("异常")
+
+    
+    
+    
+    
+    
+    
+try:    # 捕获全部异常
+    1 / 0       # print(1)
+except Exception as e:
+    print("出现异常了")
+else:
+    print("没有异常")
+finally:
+    print("无论如何都输出")    
