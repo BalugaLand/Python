4a17d674f5196c128ae3893c9ddf4fe65f787f58
BalugaLand <114586562+BalugaLand@users.noreply.github.com>
2023-03-24 16:45:01 +0800

    Update 胚子.py

--- 胚子.py
+++ 胚子.py
@@ -22,10 +22,32 @@ gjl_2 = gjl_1[a[7]]
 fyl_2 = fyl_1[a[8]]
 smz_2 = smz_1[a[9]]
 yscnxl_2 =yscnxl_1[a[10]]
+
+bl = "暴击率+"
+bs = "暴击伤害+"
+jt = "元素精通+"
+GJL = "攻击力(%)+"
+FYL = "防御力(%)+"
+SMZ = "生命值(%)+"
+gjl = "攻击力+"
+fyl = "防御力+"
+smz = "生命值+"
+yscnxl = "元素充能效率+"
+
 peizi=[]
 citiaoshu = random.randint(3,4)
-citiao_1 = [f"暴击率+{bjl_2}" ,f"暴击伤害+{bjsh_2}%",f"元素精通+{ysjt_2}",f"攻击力+{GJL_2}%", f"防御力+{FYL_2}%" ,
-            f"生命值+{SMZ_2}%" ,f"攻击力+{gjl_2}" ,f"防御力+{fyl_2}",f"生命值+{smz_2}",f"元素充能效率+{yscnxl_2}%"]
-Peizi = random.sample(citiao_1,k=citiaoshu)
-Peizi = "\n".join(Peizi)
-print(Peizi)
+citiao_1 = [f"{bl}{bjl_2}%" ,f"{bs}{bjsh_2}%",f"{jt}{ysjt_2}",f"{GJL}{GJL_2}%", f"{FYL}{FYL_2}%" ,
+            f"{SMZ}{SMZ_2}%" ,f"{gjl}{gjl_2}" ,f"{fyl}{fyl_2}",f"{smz}{smz_2}",f"{yscnxl}{yscnxl_2}%"]
+peizi = random.sample(citiao_1,k=citiaoshu)
+for i in range(citiaoshu):
+    if peizi[i-1] == f"{GJL}{GJL_2}%":
+        peizi[i-1] = f"攻击力+{GJL_2}%"
+    elif peizi[i-1] == f"{FYL}{FYL_2}%":
+        peizi[i-1] = f"防御力+{FYL_2}%"
+    elif peizi[i-1] == f"{SMZ}{SMZ_2}%":
+        peizi[i-1] = f"生命值+{SMZ_2}%"
+    else:
+        pass
+Peizi = "\n".join(peizi)
+print(Peizi,"\n")
+print("强化一次...\n")
